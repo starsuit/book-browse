cd89500318bc775ef0acb0b78f84b44094dd9eb1
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-22 20:07:02 +0100

    initial boilerplate django setup

---
+++ book_browse/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BookBrowseConfig(AppConfig):
+    name = 'book_browse'
---
+++ book_browse/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+# 'from . import x' means import relative to this file - __init__.py made this directory a module
+from . import views
+
+urlpatterns = [
+    path("", views.index, name='index')
+]
