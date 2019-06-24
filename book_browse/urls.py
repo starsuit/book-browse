2b5fc1891393c1a58ac0f70a5de798126ba1d76a
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-24 17:27:50 +0100

    write new books url path

--- book_browse/urls.py
+++ book_browse/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path('books/', views.books, name='books')
 ]
