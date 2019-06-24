b861dd51b8bcf56d8ada0958e770775fbdd2425a
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-24 12:32:11 +0100

    test named url 'index' renders

--- book_browse/tests.py
+++ book_browse/tests.py
@@ -1,5 +1,6 @@
 from django.test import SimpleTestCase
 from django.http import HttpRequest
+from django.urls import reverse
 
 class IndexTests(SimpleTestCase):
 
@@ -7,4 +8,7 @@ class IndexTests(SimpleTestCase):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
 
-# Create your tests here.
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEquals(response.status_code, 200)
+
