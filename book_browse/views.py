2d48230b3193dd7f044e0848d4bda9084f713ef7
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-24 13:04:16 +0100

    write logic to fetch data from google books api

--- book_browse/views.py
+++ book_browse/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+import requests
+from string import Template
 
 
 def index(request):
-    return HttpResponse('Hello world')
+
+    string = 'world'
+
+    queries = {'q': string}
+# r seems to be the convention
+    r = requests.get(
+        'https://www.googleapis.com/books/v1/volumes', params=queries)
+
+    books = r.json()['items']
+
+    return render(request, 'book_browse/index.html', {'books': books})
+    # return HttpResponse('Hello world')
