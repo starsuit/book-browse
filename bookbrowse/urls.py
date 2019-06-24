b3860b4e825e0eb736be998bf8f03268eadd6d8d
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-24 12:19:33 +0100

    fix urlpatterns home route typo

--- bookbrowse/urls.py
+++ bookbrowse/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/', include('book_browse.urls')),
+    path('', include('book_browse.urls')),
     path('admin/', admin.site.urls),
 ]
