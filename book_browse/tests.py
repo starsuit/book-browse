38109a8879b4b33e6210a31ab33c6993a84da514
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-24 17:27:30 +0100

    write basic tests for book list page

--- book_browse/tests.py
+++ book_browse/tests.py
@@ -18,11 +18,36 @@ class IndexTests(SimpleTestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'book_browse/index.html')
 
-    def test_index_renders_html(self):
+    def test_index_renders_title(self):
         response = self.client.get('/')
         self.assertContains(response, '<h1>Book Browse</h1>')
 
     def test_index_does_not_contain_incorrect_html(self):
         response = self.client.get('/')
+        self.assertNotContains(
+            response, 'Blah blah blah, incorrect text blah')
+
+
+class BookListTests(SimpleTestCase):
+
+    def test_books_status_code(self):
+        response = self.client.get('/books/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('books'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('books'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'book_browse/books.html')
+
+    def test_books_renders_html(self):
+        response = self.client.get('/books/')
+        self.assertContains(response, '<h1>Book Browse</h1>')
+
+    def test_books_does_not_contain_incorrect_html(self):
+        response = self.client.get('/books/')
         self.assertNotContains(
             response, 'Blah blah blah, incorrect text blah')
