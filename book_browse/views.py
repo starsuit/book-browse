c29763f06fc1e45da8dc1ef4f4a709492aca7731
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-24 13:25:20 +0100

    add api key to request

--- book_browse/views.py
+++ book_browse/views.py
@@ -3,13 +3,18 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 import requests
 from string import Template
+import environ
+
+env = environ.Env()
+env.read_env()  # reading .env file
 
 
 def index(request):
 
     string = 'world'
+    key = env.str('API_KEY')
 
-    queries = {'q': string}
+    queries = {'q': string, 'key': key}
 # r seems to be the convention
     r = requests.get(
         'https://www.googleapis.com/books/v1/volumes', params=queries)
@@ -17,4 +22,3 @@ def index(request):
     books = r.json()['items']
 
     return render(request, 'book_browse/index.html', {'books': books})
-    # return HttpResponse('Hello world')
