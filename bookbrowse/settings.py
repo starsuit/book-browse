ab8ee473482ef70dacf99f762f76ab529a76d869
Samantha Dickeson <samlucydickeson@gmail.com>
2019-07-08 17:08:22 +0100

    fix static files for production

--- bookbrowse/settings.py
+++ bookbrowse/settings.py
@@ -35,6 +35,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
+
 ALLOWED_HOSTS = []
 
 
@@ -128,6 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join('static'), )
 
 django_heroku.settings(locals())
